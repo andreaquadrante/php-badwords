3a74826f05d45dcfef6f50c6511e232503650fe0
Andrea <quadranteandrea@gmail.com>
2020-12-22 16:59:52 +0100

    str_replace

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$caption = "Suca a Palermo è una delle espressioni dialettali più usate. Un termine che in sole due sillabe si pone come sintesi di un non nulla, un qualcosa d'indefinito, che non conosce genere o classe sociale. Un'offesa che colpisce in maniera diretta e che a volte, rimane impressa sui muri.";
+
+$caption = str_replace('Suca', '***', $caption);
+
+?>
+
+<p><?php echo $caption ?></p>
