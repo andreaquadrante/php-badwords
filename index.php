c8a1276466abc068fb6771f1640b7846cdfd6ed8
Andrea <quadranteandrea@gmail.com>
2020-12-22 17:29:08 +0100

    adds var_dump and $_GET

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
-$caption = "Suca a Palermo è una delle espressioni dialettali più usate. Un termine che in sole due sillabe si pone come sintesi di un non nulla, un qualcosa d'indefinito, che non conosce genere o classe sociale. Un'offesa che colpisce in maniera diretta e che a volte, rimane impressa sui muri.";
+$badword = $_GET["badword"];
 
-$caption = str_replace('Suca', '***', $caption);
+$caption = "$badword a Palermo è una delle espressioni dialettali più usate. Un termine che in sole due sillabe si pone come sintesi di un non nulla, un qualcosa d'indefinito, che non conosce genere o classe sociale. Un'offesa che colpisce in maniera diretta e che a volte, rimane impressa sui muri.";
+
+var_dump($caption);
+
+$caption = str_replace($badword, '***', $caption);
 
 ?>
 
